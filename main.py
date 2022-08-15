72faa56346e6209bb90380b3e5bc1ff7aa9f23f9
shaun.mathew <shaunmathew123@gmail.com>
2022-08-15 13:33:49 -0400

    Removed delete on reaction code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from dotenv import load_dotenv
 from unalix import clear_url
 
+
 class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
@@ -11,12 +12,12 @@ class MyClient(discord.Client):
             # Suppress embeds for bot messages to avoid visual clutter
             if permissions.manage_messages:
                 await message.edit(suppress=True)
-                # Add :wastebasket: emoji for easy deletion if necessary
+
                 if permissions.add_reactions and permissions.read_message_history:
-                    await message.add_reaction('🗑')
+                    await message.add_reaction("🗑")
 
         # Extract links and clean
-        urls = re.findall('(?P<url>https?://[^\s]+)', message.content)
+        urls = re.findall("(?P<url>https?://[^\s]+)", message.content)
         cleaned = []
         for url in urls:
             if clear_url(url) != url:
@@ -24,20 +25,13 @@ class MyClient(discord.Client):
 
         # Send message and add reactions
         if cleaned:
-            text = 'It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n' + '\n'.join(cleaned)
+            text = (
+                "It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n"
+                + "\n".join(cleaned)
+            )
             await message.reply(text, mention_author=False)
 
-    async def on_reaction_add(self, reaction, user):
-        message = reaction.message
-        # Delete message if original author clicked on trash reaction
-        permissions = message.channel.guild.me.permissions_in(message.channel)
-        if not permissions.manage_messages or message.reference is None:
-            return
-        channel = client.get_channel(message.reference.channel_id)
-        original = await channel.fetch_message(message.reference.message_id)
-        if message.author == client.user and user == original.author and reaction.emoji == '🗑':
-            await reaction.message.delete()
 
 load_dotenv()
 client = MyClient()
-client.run(os.environ['TOKEN'])
+client.run(os.environ["TOKEN"])
